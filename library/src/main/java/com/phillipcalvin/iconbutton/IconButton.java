0701cd9ca47219bc6bd63633eccf782725a0c536
msdx <645079761@qq.com>
2014-12-24 15:25:09 +0800

    Reformat library code.

--- library/src/main/java/com/phillipcalvin/iconbutton/IconButton.java
+++ library/src/main/java/com/phillipcalvin/iconbutton/IconButton.java
@@ -2,104 +2,104 @@ package com.phillipcalvin.iconbutton;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Rect;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.Button;
 
 public class IconButton extends Button {
-  protected int drawableWidth;
-  protected DrawablePositions drawablePosition;
-  protected int iconPadding;
+    protected int drawableWidth;
+    protected DrawablePositions drawablePosition;
+    protected int iconPadding;
 
-  // Cached to prevent allocation during onLayout
-  Rect bounds;
- 
-  private enum DrawablePositions {
-    NONE,
-    LEFT,
-    RIGHT
-  }
+    // Cached to prevent allocation during onLayout
+    Rect bounds;
 
-  public IconButton(Context context) {
-    super(context);
-    bounds = new Rect();
-  }
-
-  public IconButton(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    bounds = new Rect();
-    applyAttributes(attrs);
-  }
-
-  public IconButton(Context context, AttributeSet attrs, int defStyle) {
-    super(context, attrs, defStyle);
-    bounds = new Rect();
-    applyAttributes(attrs);
-  }
-
-  protected void applyAttributes(AttributeSet attrs) {
-    // Slight contortion to prevent allocating in onLayout
-    if (null == bounds) {
-      bounds = new Rect();
+    private enum DrawablePositions {
+        NONE,
+        LEFT,
+        RIGHT
     }
 
-    SafeParcelHelper parcel = new SafeParcelHelper("com.phillipcalvin.iconbutton", 
-                                                   getContext(), 
-                                                   "com.phillipcalvin.iconbutton");
-
-    int[] styleable = parcel.getStyleableArray("IconButton");
-    TypedArray typedAttrs = getContext().obtainStyledAttributes(attrs,
-        styleable,
-        0, 0);
-    int paddingId = parcel.getStyleableId("IconButton", "iconPadding");
-    setIconPadding(typedAttrs.getDimensionPixelSize(paddingId, 0));
-    typedAttrs.recycle();
-  }
-
-  public void setIconPadding(int padding) {
-    iconPadding = padding;
-    requestLayout();
-  }
-
-  @Override
-  protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-    super.onLayout(changed, left, top, right, bottom);
-
-    Paint textPaint = getPaint();
-    String text = getText().toString();
-    textPaint.getTextBounds(text, 0, text.length(), bounds);
-
-    int textWidth = bounds.width();
-    int contentWidth = drawableWidth + iconPadding + textWidth;
-
-    int contentLeft = (int)((getWidth() / 2.0) - (contentWidth / 2.0));
-    setCompoundDrawablePadding(-contentLeft + iconPadding);
-    switch (drawablePosition) {
-    case LEFT:
-      setPadding(contentLeft, 0, 0, 0);
-      break;
-    case RIGHT:
-      setPadding(0, 0, contentLeft, 0);
-      break;
-    default:
-      setPadding(0, 0, 0, 0);
+    public IconButton(Context context) {
+        super(context);
+        bounds = new Rect();
     }
-  }
 
-  @Override
-  public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom) {
-    super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
-    if (null != left) {
-      drawableWidth = left.getIntrinsicWidth();
-      drawablePosition = DrawablePositions.LEFT;
-    } else if (null != right) {
-      drawableWidth = right.getIntrinsicWidth();
-      drawablePosition = DrawablePositions.RIGHT;
-    } else {
-      drawablePosition = DrawablePositions.NONE;
+    public IconButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        bounds = new Rect();
+        applyAttributes(attrs);
     }
-    requestLayout();
-  }
+
+    public IconButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        bounds = new Rect();
+        applyAttributes(attrs);
+    }
+
+    protected void applyAttributes(AttributeSet attrs) {
+        // Slight contortion to prevent allocating in onLayout
+        if (null == bounds) {
+            bounds = new Rect();
+        }
+
+        SafeParcelHelper parcel = new SafeParcelHelper("com.phillipcalvin.iconbutton",
+                getContext(),
+                "com.phillipcalvin.iconbutton");
+
+        int[] styleable = parcel.getStyleableArray("IconButton");
+        TypedArray typedAttrs = getContext().obtainStyledAttributes(attrs,
+                styleable,
+                0, 0);
+        int paddingId = parcel.getStyleableId("IconButton", "iconPadding");
+        setIconPadding(typedAttrs.getDimensionPixelSize(paddingId, 0));
+        typedAttrs.recycle();
+    }
+
+    public void setIconPadding(int padding) {
+        iconPadding = padding;
+        requestLayout();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        Paint textPaint = getPaint();
+        String text = getText().toString();
+        textPaint.getTextBounds(text, 0, text.length(), bounds);
+
+        int textWidth = bounds.width();
+        int contentWidth = drawableWidth + iconPadding + textWidth;
+
+        int contentLeft = (int) ((getWidth() / 2.0) - (contentWidth / 2.0));
+        setCompoundDrawablePadding(-contentLeft + iconPadding);
+        switch (drawablePosition) {
+            case LEFT:
+                setPadding(contentLeft, 0, 0, 0);
+                break;
+            case RIGHT:
+                setPadding(0, 0, contentLeft, 0);
+                break;
+            default:
+                setPadding(0, 0, 0, 0);
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        if (null != left) {
+            drawableWidth = left.getIntrinsicWidth();
+            drawablePosition = DrawablePositions.LEFT;
+        } else if (null != right) {
+            drawableWidth = right.getIntrinsicWidth();
+            drawablePosition = DrawablePositions.RIGHT;
+        } else {
+            drawablePosition = DrawablePositions.NONE;
+        }
+        requestLayout();
+    }
 }
--- library/src/main/java/com/phillipcalvin/iconbutton/SafeParcelHelper.java
+++ library/src/main/java/com/phillipcalvin/iconbutton/SafeParcelHelper.java
@@ -1,25 +1,26 @@
 package com.phillipcalvin.iconbutton;
 
 /***
-Copyright (c) 2010 CommonsWare, LLC
+ Copyright (c) 2010 CommonsWare, LLC
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may
-not use this file except in compliance with the License. You may obtain
-a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-Modified to handle contexts without a package name using a fallback Package.
-This allows widgets accessing resources to be edited in the Eclipse/ADT layout tool.
-Phil Calvin
+ Modified to handle contexts without a package name using a fallback Package.
+ This allows widgets accessing resources to be edited in the Eclipse/ADT layout tool.
+ Phil Calvin
 
- */    
+ */
 
 import android.content.Context;
+
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.Collections;
@@ -27,141 +28,138 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SafeParcelHelper {
-  private static final int CACHE_SIZE=101;
-  private String id=null;
-  private Class arrr=null;
-  private Map<String, Integer> cache=null;
+    private static final int CACHE_SIZE = 101;
+    private String id = null;
+    private Class arrr = null;
+    private Map<String, Integer> cache = null;
 
-  public SafeParcelHelper(String id, Context ctxt, String fallbackPackage) {
-    this.id=id.replace('.','_').replace('-','_');
-    String packageName = ctxt.getPackageName();
-    // Package name will be null inside the Eclipse/ADT layout tool
-    if (null == packageName) {
-      packageName = fallbackPackage;
+    public SafeParcelHelper(String id, Context ctxt, String fallbackPackage) {
+        this.id = id.replace('.', '_').replace('-', '_');
+        String packageName = ctxt.getPackageName();
+        // Package name will be null inside the Eclipse/ADT layout tool
+        if (null == packageName) {
+            packageName = fallbackPackage;
+        }
+
+        try {
+            this.arrr = Class.forName(packageName + ".R");
+        } catch (Throwable t) {
+            throw new RuntimeException("Exception finding R class", t);
+        }
+
+        this.cache = Collections.synchronizedMap(new Cache());
     }
 
-    try {
-      this.arrr=Class.forName(packageName+".R");
-    }
-    catch (Throwable t) {
-      throw new RuntimeException("Exception finding R class", t);
-    }
+    public int[] getStyleableArray(String name) {
+        try {
+            Class clazz = getResourceClass("styleable");
 
-    this.cache=Collections.synchronizedMap(new Cache());
-  }
+            if (clazz != null) {
+                Field fld = clazz.getDeclaredField(name);
 
-  public int[] getStyleableArray(String name) {
-    try {
-      Class clazz=getResourceClass("styleable");
+                if (fld != null) {
+                    Object o = fld.get(clazz);
 
-      if (clazz!=null) {
-        Field fld=clazz.getDeclaredField(name);
+                    if (o instanceof int[]) {
+                        int[] result = new int[Array.getLength(o)];
 
-        if (fld!=null) {
-          Object o=fld.get(clazz);
+                        for (int i = 0; i < Array.getLength(o); i++) {
+                            result[i] = Array.getInt(o, i);
+                        }
 
-          if (o instanceof int[]) {
-            int[] result=new int[Array.getLength(o)];
-
-            for (int i=0;i<Array.getLength(o);i++) {
-              result[i]=Array.getInt(o, i);
+                        return (result);
+                    }
+                }
             }
-
-            return(result);
-          }
+        } catch (Throwable t) {
+            throw new RuntimeException("Exception finding styleable", t);
         }
-      }
-    }
-    catch (Throwable t) {
-      throw new RuntimeException("Exception finding styleable", t);
+
+        return (new int[0]);
     }
 
-    return(new int[0]);
-  }
-
-  public int getStyleableId(String component, String attr) {
-    return(getIdentifier(component+"_"+attr, "styleable", false));
-  }
-
-  public int getLayoutId(String layout) {
-    return(getIdentifier(layout, "layout", true));
-  }
-
-  public int getItemId(String item) {
-    return(getIdentifier(item, "id", false));
-  }
-
-  public int getMenuId(String menu) {
-    return(getIdentifier(menu, "menu", true));
-  }
-
-  public int getDrawableId(String drawable) {
-    return(getIdentifier(drawable, "drawable", true));
-  }
-
-  public int getIdentifier(String name, String defType) {
-    return(getIdentifier(name, defType, true));
-  }
-
-  public int getIdentifier(String name, String defType,
-      boolean mungeName) {
-    int result=-1;
-    StringBuilder cacheKey=new StringBuilder(name);
-
-    cacheKey.append('|');
-    cacheKey.append(defType);
-
-    Integer cacheHit=cache.get(cacheKey.toString());
-
-    if (cacheHit!=null) {
-      return(cacheHit.intValue());
+    public int getStyleableId(String component, String attr) {
+        return (getIdentifier(component + "_" + attr, "styleable", false));
     }
 
-    if (!name.startsWith(id) && mungeName) {
-      StringBuilder buf=new StringBuilder(id);
-
-      buf.append('_');
-      buf.append(name);
-
-      name=buf.toString();
+    public int getLayoutId(String layout) {
+        return (getIdentifier(layout, "layout", true));
     }
 
-    try {
-      Class clazz=getResourceClass(defType);
+    public int getItemId(String item) {
+        return (getIdentifier(item, "id", false));
+    }
 
-      if (clazz!=null) {
-        Field fld=clazz.getDeclaredField(name);
+    public int getMenuId(String menu) {
+        return (getIdentifier(menu, "menu", true));
+    }
 
-        if (fld!=null) {
-          result=fld.getInt(clazz);
-          cache.put(cacheKey.toString(), result);
+    public int getDrawableId(String drawable) {
+        return (getIdentifier(drawable, "drawable", true));
+    }
+
+    public int getIdentifier(String name, String defType) {
+        return (getIdentifier(name, defType, true));
+    }
+
+    public int getIdentifier(String name, String defType,
+                             boolean mungeName) {
+        int result = -1;
+        StringBuilder cacheKey = new StringBuilder(name);
+
+        cacheKey.append('|');
+        cacheKey.append(defType);
+
+        Integer cacheHit = cache.get(cacheKey.toString());
+
+        if (cacheHit != null) {
+            return (cacheHit.intValue());
         }
-      }
-    }
-    catch (Throwable t) {
-      throw new RuntimeException("Exception finding resource identifier", t);
+
+        if (!name.startsWith(id) && mungeName) {
+            StringBuilder buf = new StringBuilder(id);
+
+            buf.append('_');
+            buf.append(name);
+
+            name = buf.toString();
+        }
+
+        try {
+            Class clazz = getResourceClass(defType);
+
+            if (clazz != null) {
+                Field fld = clazz.getDeclaredField(name);
+
+                if (fld != null) {
+                    result = fld.getInt(clazz);
+                    cache.put(cacheKey.toString(), result);
+                }
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException("Exception finding resource identifier", t);
+        }
+
+        return (result);
     }
 
-    return(result);
-  }
+    private Class getResourceClass(String defType) {
+        for (Class clazz : arrr.getClasses()) {
+            if (defType.equals(clazz.getSimpleName())) {
+                return (clazz);
+            }
+        }
 
-  private Class getResourceClass(String defType) {
-    for (Class clazz : arrr.getClasses()) {
-      if (defType.equals(clazz.getSimpleName())) {
-        return(clazz);
-      }
+        return (null);
     }
 
-    return(null);
-  }
+    public class Cache extends LinkedHashMap<String, Integer> {
+        public Cache() {
+            super(CACHE_SIZE, 1.1f, true);
+        }
 
-  public class Cache extends LinkedHashMap<String,Integer> {
-    public Cache() {
-      super(CACHE_SIZE, 1.1f, true);
+        protected boolean removeEldestEntry(Entry eldest) {
+            return (size() > CACHE_SIZE);
+        }
     }
-
-    protected boolean removeEldestEntry(Entry eldest) {
-      return(size()>CACHE_SIZE);
-    }
-  }
 }
